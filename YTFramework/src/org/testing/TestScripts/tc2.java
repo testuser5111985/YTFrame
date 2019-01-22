4e2c4507b1cab0fd91883993b0925a876c988687
smrati.rai <smrati.rai@LPCLH-5CD8032ZQW.espire.com>
2019-01-22 17:09:08 +0530

    first commit

---
+++ YTFramework/src/org/testing/TestScripts/tc2.java
@@ -0,0 +1,13 @@
+package org.testing.TestScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+
+public class tc2 {
+	@BeforeMethod
+	public void lunchurl() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\smrati.rai\\Desktop\\selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+	}
+}
